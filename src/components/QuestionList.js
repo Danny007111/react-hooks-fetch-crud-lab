563120996c484cd59cb7d08f477a10922c60c437
Danny Rodriguez <danny007111@hotmail.com>
2022-06-17 12:56:01 -0500

    Fetch get completed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([]);
+
+  // Get from fetch
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((question) => setQuestions(question));
+  }, []);
+
+  const questionItems = questions.map((q) => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+      />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+      {questionItems}
+      </ul>
     </section>
   );
 }
